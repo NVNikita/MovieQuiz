ee2c1c109d02264be1fca0372ec60a3c68d9c1cd
Nikita Nagornyi <NVNagornyi@yandex.ru>
2024-10-23 21:21:07 +0300

    Проект на ревью

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,6 +1,7 @@
 import UIKit
 
 extension UIColor {
+    // Colors for fonts
     static var YPGreen: UIColor { UIColor(named: "YP Black") ?? UIColor.green }
     static var YPRed: UIColor { UIColor(named: "YP Red") ?? UIColor.red }
     static var YPBlack: UIColor { UIColor(named: "YP Black") ?? UIColor.black }
